cf2a3afc6a7eb3e52d004624c3a517c4ad56cb52
abdelrhman-hegazy <hegazy012075976@gmail.com>
2026-03-22 10:19:14 +0200

    feet(chat): profile.repository.prisma (find, create, update), improvement user.repository.prisma

--- src/modules/profile/domain/repository/profile.repository.ts
+++ src/modules/profile/domain/repository/profile.repository.ts
@@ -1,7 +1,16 @@
 import { Profile } from "../entities/profile.entity";
 
+export interface ProfileData {
+  id: string;
+  userId: string;
+  name: string;
+  bio: string;
+  avatar: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 export interface ProfileRepository {
-  findProfileByUserId(userId: string): Promise<Profile | null>;
-  createProfile(userId: string): Promise<Profile>;
-  updateProfile(profile: Profile): Promise<void>;
+  findProfileByUserId(userId: string): Promise<ProfileData | null>;
+  createProfile(userId: string): Promise<ProfileData>;
+  updateProfile(profile: Profile): Promise<ProfileData>;
 }
